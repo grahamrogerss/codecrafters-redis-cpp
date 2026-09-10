e18176799f9348e4e99eac46d745001607fcd30b
Graham Rogers <grahamrogers619@gmail.com>
2026-09-10 19:04:16 -0300

    codecrafters submit [skip ci]

--- src/main.cpp
+++ src/main.cpp
@@ -348,6 +348,10 @@ int main(int argc, char **argv) {
             else if (command == "REPLCONF") {
               write(polls[i].fd, "+OK\r\n", 5);
             }
+            else if (command == "PSYNC") {
+              sdt::string response = "+FULLRESYNC " + replid + "0\r\n";
+              write(polls[i].fd, response.c_str(), response.length());
+            }
             else {
               write(polls[i].fd, "+PONG\r\n", 7);
             }
